caf3633607068960eb0c971797c07cb0a3dd242c
hdigital <hdigital@users.noreply.github.com>
2023-02-25 15:54:08 +0100

    Revise Python code moviestar
    + add Jupyter code cells
    + order module import
    + correct dataframe name
    + show model output
    + move plot into function
    + coherent spacing

--- python/moviestar.py
+++ python/moviestar.py
@@ -1,37 +1,60 @@
-import numpy as np 
-import pandas as pd 
-import statsmodels.api as sm 
-import statsmodels.formula.api as smf 
-from itertools import combinations 
-import plotnine as p
-from stargazer.stargazer import Stargazer
+# %% -- import modules
+from itertools import combinations
 
-# read data
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import plotnine as p9
+
+
+# %% -- read data
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
-def read_data(file): 
+def read_data(file):
 	return pd.read_stata("https://github.com/scunning1975/mixtape/raw/master/" + file)
 
 
 
-start_is_born = pd.DataFrame({
-    'beauty': np.random.normal(size=2500),
-    'talent': np.random.normal(size=2500)})
-    
-start_is_born['score'] = start_is_born['beauty'] + start_is_born['talent']
-start_is_born['c85'] = np.percentile(start_is_born['score'], q=85)
-start_is_born['star'] = 0
-start_is_born.loc[start_is_born['score']>start_is_born['c85'], 'star'] = 1
-start_is_born.head()
+# %% -- create dataset
+star_is_born = pd.DataFrame(
+	{
+        'beauty': np.random.normal(size=2500),
+        'talent': np.random.normal(size=2500)
+    }
+)
+
+star_is_born['score'] = star_is_born['beauty'] + star_is_born['talent']
+star_is_born['c85'] = np.percentile(star_is_born['score'], q=85)
+star_is_born['star'] = 0
+star_is_born.loc[star_is_born['score'] > star_is_born['c85'], 'star'] = 1
+star_is_born.head()
 
 
-lm = sm.OLS.from_formula('beauty ~ talent', data=start_is_born).fit()
+# %% -- linear model // all people
+lm_1 = sm.OLS.from_formula('beauty ~ talent', data=star_is_born).fit()
+print(lm_1.summary())
 
-p.ggplot(start_is_born, p.aes(x='talent', y='beauty')) +    p.geom_point(size = 0.5) +    p.xlim(-4, 4) +    p.ylim(-4, 4)
-
-p.ggplot(start_is_born[start_is_born.star==1], p.aes(x='talent', y='beauty')) +    p.geom_point(size = 0.5) +    p.xlim(-4, 4) +    p.ylim(-4, 4)
-
-p.ggplot(start_is_born[start_is_born.star==0], p.aes(x='talent', y='beauty')) +    p.geom_point(size = 0.5) +    p.xlim(-4, 4) +    p.ylim(-4, 4)
+# %% -- linear model // interaction effect
+lm_2 = sm.OLS.from_formula('beauty ~ talent * star', data=star_is_born).fit()
+print(lm_2.summary())
 
 
+# %% -- plot
 
+def plot_data(data, title = ""):
+    plot = (
+        p9.ggplot(data, p9.aes(x='talent', y='beauty')) +
+            p9.geom_point(size=0.5) +
+            p9.geom_smooth(method="lm", se=False, colour="blue") +
+            p9.xlim(-4, 4) +
+            p9.ylim(-4, 4) +
+            p9.labs(title=title)
+    )
+    return plot
+
+# %%
+plot_data(star_is_born, "All people")
+# %%
+plot_data(star_is_born[star_is_born.star == 1], "Other people")
+# %%
+plot_data(star_is_born[star_is_born.star == 0], "Stars")
